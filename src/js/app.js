f280a4ede5d157e25aa61a8812f2bc707082c931
Axel Orn Sigurdsson <axel@takumi.com>
2015-10-21 23:41:38 +0000

    Implement unsubscribe

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ let isPushEnabled = false;
 const sendSubscriptionToServer = (subscription) => {
   fetch('https://test-portal.absalon.is', {method: 'post', body: subscription.endpoint});
 };
+
 const subscribe = () => {
   // Disable the button so it can't be changed while
   // we process the permission request
@@ -35,7 +36,44 @@ const subscribe = () => {
       });
   });
 };
-const unsubscribe = () => {};
+
+const unsubscribe = () => {
+  const pushButton = document.querySelector('.js-push-button');
+  pushButton.disabled = true;
+
+  navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+    serviceWorkerRegistration.pushManager.getSubscription().then((pushSubscription) => {
+      // Check we have a subcription to unsubscribe
+      if (!pushSubscription) {
+        // No subscription object, set state to allow to push
+        isPushEnabled = false;
+        pushButton.disabled = false;
+        pushButton.textContent = 'Enable Push Messages';
+        return;
+      }
+
+      // const subscriptionId = pushSubscription.subscriptionId;
+      // TODO: Make a request to server to remove id
+
+      // We have a subscription, so call unsubscribe on it
+      pushSubscription.unsubscribe().then(() => {
+        pushButton.disabled = false;
+        pushButton.textContent = 'Enable Push Messages';
+        isPushEnabled = false;
+      })
+      .catch((e) => {
+        // Failed to unsubscribe.. remove user data from our data store
+        // and let the user know we did so
+        console.log('Unsubscription error: ', e);
+        pushButton.disabled = false;
+        pushButton.textContent = 'Enable Push Messages';
+      });
+    })
+    .catch((e) => {
+      console.error('Error thrown while unsubscribing from push messaging.', e);
+    });
+  });
+};
 
 const initialState = () => {
   // Are Notifications supported in the service worker?
