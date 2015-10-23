1ac8324d5ba5c463ccf6f63a12a70a373cf5629a
Axel Orn Sigurdsson <axel@takumi.com>
2015-10-23 20:36:42 +0000

    Add support to add events

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fetch = require('node-fetch');
 const app = express();
 const db = new sqlite3.Database('canaritus.db');
 db.run('CREATE TABLE IF NOT EXISTS ids (id TEXT, UNIQUE(id))');
+db.run('CREATE TABLE IF NOT EXISTS events (title TEXT, body TEXT, time DATETIME)');
 
 const PORT = 3000;
 const SERVER_KEY = process.env.SERVER_KEY;
@@ -14,6 +15,30 @@ const GCM_ENDPOINT = 'https://android.googleapis.com/gcm/send';
 if (!SERVER_KEY) {
   console.log('SERVER_KEY env variable not set, will not be able to send push notification');
 }
+
+const pingClients = () => {
+  console.log('Pinging clients...');
+  db.all('SELECT * FROM ids', (err, rows) => {
+    if (err !== null) {
+      console.log('Failed to select registration ids');
+    } else {
+      const ids = rows.map(x => x.id);
+      fetch(GCM_ENDPOINT, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `key=${SERVER_KEY}`,
+        },
+        body: JSON.stringify({
+          'registration_ids': ids,
+        }),
+      }).then((res) => {
+        console.log('Notification pings sent, status:', res.status);
+      });
+    }
+  });
+};
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
@@ -24,7 +49,7 @@ app.post('/subscribe', (req, res) => {
     res.status(400).send('id is missing from the post');
   } else {
     console.log(`Adding ${id} to ids table`);
-    db.run(`INSERT OR IGNORE INTO 'ids' (id) VALUES('${id}')`, (err) => {
+    db.run(`INSERT OR IGNORE INTO ids (id) VALUES('${id}')`, (err) => {
       if (err !== null) {
         console.log('Error adding id', err);
         res.sendStatus(500);
@@ -52,24 +77,33 @@ app.post('/unsubscribe', (req, res) => {
   }
 });
 
-app.get('/send_notification', (req, res) => {
-  db.all('SELECT * FROM ids', (err, rows) => {
+app.post('/event', (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  if (!title || !body) {
+    res.status(400).send('title and/or body missing from the post');
+  } else {
+    console.log(`Adding event "${title}: ${body}"`);
+    db.run(`INSERT INTO events (title, body, time) VALUES('${title}', '${body}', datetime('now'))`, (err) => {
+      if (err !== null) {
+        console.log('Error adding event', err);
+        res.sendStatus(500);
+      } else {
+        pingClients();
+        res.sendStatus(201);
+      }
+    });
+  }
+});
+
+app.get('/event', (req, res) => {
+  db.get('SELECT * FROM events ORDER BY time DESC LIMIT 1', (err, event) => {
     if (err !== null) {
-      res.status(500).send('Failed to select all rows?');
+      res.status(500).send('Failed to get latest event');
     } else {
-      const ids = rows.map(x => x.id);
-      fetch(GCM_ENDPOINT, {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': `key=${SERVER_KEY}`,
-        },
-        body: JSON.stringify({
-          'registration_ids': ids,
-        }),
-      }).then((fRes) => {
-        res.status(200).send(fRes);
+      res.status(200).json({
+        title: event.title,
+        body: event.body,
       });
     }
   });
