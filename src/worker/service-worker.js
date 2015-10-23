392da9963e37840b8c2cfe4d1e68b6f3d0fbcab1
Axel Orn Sigurdsson <axel@takumi.com>
2015-10-23 21:22:05 +0000

    Add notification click handler for android

--- src/worker/service-worker.js
+++ src/worker/service-worker.js
@@ -1,3 +1,5 @@
+/* eslint-env serviceworker */
+
 self.addEventListener('push', (event) => {
   const icon = '/images/icon-256x256.png';
   event.waitUntil(fetch('/event').then((res) => {
@@ -29,5 +31,28 @@ self.addEventListener('push', (event) => {
       icon: icon,
       tag: tag,
     });
+  }));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window',
+  }).then((clientList) => {
+    for (let i = 0; i < clientList.length; i++) {
+      const client = clientList[i];
+      if (client.url === '/' && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow('/');
+    }
   }));
 });
