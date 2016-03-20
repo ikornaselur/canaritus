0a43e25adbbb9b7de4b59421f70f4c07f48f5ba5
Axel Orn Sigurdsson <axel@takumi.com>
2016-03-20 16:38:01 +0000

    Generate recipient variables for emails to prevent leaking emails

--- src/server/notifications/mailgun.js
+++ src/server/notifications/mailgun.js
@@ -1,30 +1,46 @@
 import {default as Mailgun} from 'mailgun-js';
 import {log} from '../utils';
 
-let mailgun;
-let from;
+let _mailgun;
+let _from;
 
+const generateEmailData = (emails, from, title, body) => {
+  // Random hash to force mailgun to send each recipient an individual email, instead of
+  // grouping them all together (and exposing emails) in the `to` header.
+  // This random hash will be replaced with a `unsubscribe` hash/url in the future
+  const randHash = (len) => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, len);
+
+  const recipientVariables = {};
+  emails.map((e) => {
+    recipientVariables[e] = {hash: randHash(10)};
+  });
+
+
+  return {
+    'from': `Canaritus <${from}>`,
+    'to': emails.join(', '),
+    'recipient-variables': JSON.stringify(recipientVariables),
+    'subject': title,
+    'html': body,
+  };
+};
 
 export const initialize = (config) => {
   if (config.enabled) {
-    mailgun = new Mailgun({
+    _mailgun = new Mailgun({
       apiKey: config.api_key,
       domain: config.domain,
     });
-    from = config.from;
+    _from = config.from;
   }
 };
 
 export const notify = (title, body) => {
-  const data = {
-    from: `Canaritus <${from}>`,
-    to: '',  // TODO: Emails?
-    subject: title,
-    html: body,
-  };
+  const emails = [];
+  const data = generateEmailData(emails, _from, title, body);
 
   log('MAILGUN', 'Sending emails');
-  mailgun.messages().send(data, (err) => {
+  _mailgun.messages().send(data, (err) => {
     if (err) {
       log('MAILGUN', 'Error sending email', err);
     }
