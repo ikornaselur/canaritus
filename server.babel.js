6936f133211470b9f137c80c58888f9db1d398ee
Axel Orn Sigurdsson <axel@takumi.com>
2016-03-20 18:41:46 +0000

    Update all dependencies

--- server.babel.js
+++ server.babel.js
@@ -1,2 +1,2 @@
-require('babel/register')({ stage: 0 });
+require('babel-core/register');
 require('./server.js');
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel-loader?stage=0'],
+      loaders: ['babel'],
       exclude: [/node_modules/, /worker/],
       include: __dirname,
     }, {
